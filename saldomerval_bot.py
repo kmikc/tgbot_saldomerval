b46b51bfb1fadc1f1ce65e2ea3dff5411584957c
jParkes <jparkes@redcalidad.com>
2017-06-27 16:17:25 -0400

    lxml para ordenar resultado

--- saldomerval_bot.py
+++ saldomerval_bot.py
@@ -8,9 +8,10 @@ import sqlite3 as lite
 import datetime
 import requests
 import logging
-import HTMLParser
-from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
+#import HTMLParser
+#from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
 import uuid
+from lxml import html
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -50,49 +51,38 @@ def get_cardnumber(p_chat_id):
 #
 
 def saldo(bot, update):
-    print '---------------'
-    print 'saldo'
-    print datetime.datetime.now()
     p_chat_id = update.message.chat.id
-    print p_chat_id
     int_cardnumber = get_cardnumber(p_chat_id)
-
-    print int_cardnumber
+    return_text = '';
 
     if int_cardnumber > -1:
         # CONSULTAR
-
-        print 'Consultar...'
-
         r = requests.post("https://www.metro-valparaiso.cl/saldonuevo.php", data={"numerotarjeta": int_cardnumber})
 
-        #r.text
-        responsetext = r.text
-        print responsetext
-        return_text = responsetext
+        tree = html.fromstring(r.content)
 
-        #HTMLParser
-        html_parser = HTMLParser.HTMLParser()
-        responsetext_unescaped = html_parser.unescape(responsetext)
-        print responsetext_unescaped
-        return_text = responsetext_unescaped
+        # Saldo, operaciones y mensaje final
+        lSaldo = tree.xpath('//h2/strong/text()')
+        lOperaciones = tree.xpath('//li//strong/text()')
+        lMensaje = tree.xpath('//p/text()')
 
-        #BeautifulSoup
-        responsetext_unescaped_beautiful = unicode(BeautifulSoup(responsetext_unescaped, convertEntities=BeautifulStoneSoup.ALL_ENTITIES).text)
-        print responsetext_unescaped_beautiful
-        return_text = responsetext_unescaped_beautiful
+        # Preparar respuesta
+        if len(lSaldo) > 0:
+            return_text = "Tu saldo disponible es: " + lSaldo[0]
+
+        if len(lOperaciones) > 0:
+
+
+        return_text = return_text + lMensaje[0]
 
     else:
         # NO HAY NUMERO DE TARJETA
-
-        print 'No hay numero'
         return_text = 'No hay número de tarjeta configurado. Ingrésalo con /numerotarjeta <numero>'
 
     bot.sendMessage(chat_id=update.message.chat.id, text=return_text)
 
 
 def numerotarjeta(bot, update, args):
-    print 'numerotarjeta'
     str_result = 'args: {}'.format(args[0])
     try:
         p_numerotarjeta = int(args[0])
@@ -113,16 +103,12 @@ def numerotarjeta(bot, update, args):
             cur.execute("SELECT COUNT(*) FROM saldomerval WHERE chatid=:CHATID", {"CHATID": update.message.chat.id})
             row_count = cur.fetchone()[0]
 
-            print row_count
-
             # Segun resultado obtenido, actualiza o inserta
             if row_count > 0:
-                print 'UPDATE'
                 cur.execute("UPDATE saldomerval SET cardnumber=?, username=? WHERE chatid=?", (p_numerotarjeta, p_username, update.message.chat.id))
                 conn.commit()
                 str_result = 'Se ha actualizado el número de la tarjeta'
             else:
-                print 'INSERT'
                 p_uniqid = str(uuid.uuid4())
                 cur.execute("INSERT INTO saldomerval (uniqid, chatid, cardnumber, username) VALUES (?, ?, ?, ?)", (p_uniqid, update.message.chat.id, p_numerotarjeta, p_username))
                 conn.commit()
@@ -136,52 +122,6 @@ def numerotarjeta(bot, update, args):
                 conn.close()
 
     bot.sendMessage(chat_id=update.message.chat.id, text=str_result)
-
-
-#
-#
-# CONFIGURA EL VALOR DEL GMT, Y LO GUARDA EN DB
-#
-#
-
-def gmt(bot, update, args):
-    str_result = 'args: {}'.format(args[0])
-    try:
-        var_gmt = int(args[0])
-        ok = True
-
-    except:
-        str_result = "Valor no numérico"
-        ok = False
-
-    if ok:
-        conn = None
-        try:
-            conn = lite.connect('checkpoint_settings.db')
-            cur = conn.cursor()
-
-            # Cuenta regstros existentes
-            cur.execute("SELECT COUNT(*) FROM chat_settings WHERE chat_id=:CHATID", {"CHATID": update.message.chat.id})
-            row_count = cur.fetchone()[0]
-
-            # Segun resultado obtenido, actualiza o inserta
-            if row_count > 0:
-                cur.execute("UPDATE chat_settings SET gmt_value=? WHERE chat_id=?", (var_gmt, update.message.chat.id))
-                conn.commit()
-                str_result = 'Registro actualizado'
-            else:
-                cur.execute("INSERT INTO chat_settings (chat_id, gmt_value) VALUES (?, ?)", (update.message.chat.id, var_gmt))
-                conn.commit()
-                str_result = 'Registro ingresado'
-
-        except:
-            str_result = 'No se pudo conectar'
-
-        finally:
-            if conn:
-                conn.close()
-
-    update.message.reply_text(str_result)
 
 
 # TOKEN
